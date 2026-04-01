1e13bc472de905a63f60b4ca8f2d11cd07675d3f
Iván Sánchez Vázquez <ivan.sanchez@toldosgomez.local>
2026-04-01 09:14:18 +0200

    cambio a puerto 4000

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    allowedDevOrigins: ['192.168.4.90'],
+    allowedDevOrigins: ['192.168.4.90:4000'],
 };
 
 export default nextConfig;
